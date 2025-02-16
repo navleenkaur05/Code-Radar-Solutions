b26103a74c5eb93080181a4d7f0e788dda514136
navleenkaur05 <navleen252@gmail.com>
2025-02-16 12:54:59 +0530

    Solution to Find Greater Number

---
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a>=b){
+        printf("%d",a);
+    }
+    else if(b>=a){
+        printf("%d",b);
+    }
+    else{
+        printf("");
+    }
+}
